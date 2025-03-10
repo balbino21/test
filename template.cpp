be268a770c17fa401c5338f2a69192fe4ca0894d
balbino21 <balbinopedrobaptista@gmail.com>
2025-03-11 04:07:31 +0700

    test

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Typedefs for convenience
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+
+// Macros
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define ff first
+#define ss second
+
+// Debugging Macro (Use for debugging during practice)
+// #define debug(x) cerr << #x << " = " << x << endl;
+
+void solve() {
+    // Your code here
+}
+
+int main() {
+    FAST_IO;
+    int t = 1; 
+    cin >> t;  // Uncomment if multiple test cases
+    while (t--) solve();
+    return 0;
+}
